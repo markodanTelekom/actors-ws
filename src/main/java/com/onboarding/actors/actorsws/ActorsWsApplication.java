57f2fc2efeb363ca130be59448cb93cd27fcfbfd
Marko Danilović <markodan@telekom.rs>
2024-03-12 09:26:03 +0100

    Initial commit

---
+++ src/main/java/com/onboarding/actors/actorsws/ActorsWsApplication.java
@@ -0,0 +1,13 @@
+package com.onboarding.actors.actorsws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onboarding/actors/actorsws/ActorsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboarding.actors.actorsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
